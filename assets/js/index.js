e87d7d5ee1cce1c774970fe4989b271bf1fee9ed
nHur2004 <nathan.hur04@gmail.com>
2022-12-17 13:22:46 -0700

    gameplan

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+var now = moment();
+currentDayE1.textContent = moment(now).format('dddd, MMMM do');
+var container = document.querySelector('.container');
+var timesArray = [ '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM' ];
+
+// create loop to make timeblocks
+
+// load timeblocks before loading storage
+
+// save button; saves to localStorage 
+
+// load from localStorage
