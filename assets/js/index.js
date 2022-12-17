3940754dcbebde148dda4b1317070df933465589
nHur2004 <nathan.hur04@gmail.com>
2022-12-17 13:35:17 -0700

    timeblocks are showing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,27 @@
 var now = moment();
-currentDayE1.textContent = moment(now).format('dddd, MMMM do');
 var container = document.querySelector('.container');
 var timesArray = [ '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM' ];
 
 // create loop to make timeblocks
+var timeBlocks = function() {
+    for ( var i = 0; i <timesArray.length; i++ ) {
+        // div that holds each time
+        var timeRows = document.createElement('div');
+        timeRows.setAttribute('class', 'row timeRows');
+        container.appendChild(timeRows);
+        // hour display
+        var hourDisplay = document.createElement('div');
+        hourDisplay.setAttribute('class', 'col-sm-1');
+        hourDisplay.textContent = timesArray[i];
+        timeRows.appendChild(hourDisplay);
+    }
+}
 
 // load timeblocks before loading storage
+timeBlocks();
 
 // save button; saves to localStorage 
 
+
 // load from localStorage
+
